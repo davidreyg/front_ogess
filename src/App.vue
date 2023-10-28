02d22b9487ab43ccbb7a9aee929bf2a8b2ba1d13
davidreyg <neisserrey@upeu.edu.pe>
2023-10-27 20:52:18 -0500

    UPDATE: Añadimos transiciones al App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
-  <router-view />
+  <router-view v-slot="{ Component }">
+    <transition
+      appear
+      mode="in-out"
+      enter-active-class="animated fadeIn"
+      leave-active-class="animated fadeOut"
+      :duration="150"
+    >
+      <component :is="Component"></component>
+    </transition>
+  </router-view>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
